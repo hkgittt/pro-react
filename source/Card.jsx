3e1029cfbafecd96f94b2bb55e7c4a68494d60a2
HK Yoon <HK Yoon>
2016-03-29 01:07:02 +0900

    Page 38

--- source/Card.jsx
+++ source/Card.jsx
@@ -1,19 +1,43 @@
 import React from 'react';
 import CheckList from './CheckList';
 
-const Card = (props) => (
-  <div className="card">
-    <div className="card__title">
-      {props.title}
-    </div>
-    <div className="card__details">
-      {props.description}
-      <CheckList cardId={props.id}
-        tasks={props.tasks}
-      />
-    </div>
-  </div>
-);
+class Card extends React.Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      showDetails: false,
+    };
+    this.toggleDetails = this.toggleDetails.bind(this);
+  }
+  toggleDetails() {
+    this.setState({
+      showDetails: !this.state.showDetails,
+    });
+  }
+  render() {
+    let cardDetails;
+    if (this.state.showDetails) {
+      cardDetails = (
+        <div className="card__details">
+          {this.props.description}
+          <CheckList cardId={this.props.id}
+            tasks={this.props.tasks}
+          />
+        </div>
+      );
+    }
+    return (
+      <div className="card">
+        <div className={
+            this.state.showDetails ? 'card__title card__title--is-open' : 'card__title'
+          }
+          onClick={this.toggleDetails} // comment
+        >{this.props.title}</div>
+        {cardDetails}
+      </div>
+    );
+  }
+}
 
 Card.propTypes = {
   title: React.PropTypes.string,
